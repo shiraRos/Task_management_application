b217d938aaadc78b02fb341dc7f8b9580a91ab95
Miri <miri105560@gmail.com>
2023-10-17 16:26:15 +0300

    creat a new program name and 2 functions

---
+++ dotNet5783_6836_4770/Stage0/Program4770.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Targil0
+{
+    partial class program
+    {
+       
+        static partial void Welcome6836();
+
+        private static void Welcome4770()
+        {
+            string nm;
+            Console.WriteLine("Enter your name:");
+            nm = Console.ReadLine();
+            Console.WriteLine("{0}, wellcome to my first console application", nm);
+        }
+        static void Main(string[] args)
+        {
+            Welcome4770();
+            Welcome6836();
+            Console.ReadKey();
+        }
+    }
+}
