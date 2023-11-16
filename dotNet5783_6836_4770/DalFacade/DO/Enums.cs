612a31622190ec86522de438a3e453412534e931
shiraRos <shira.r200@gmail.com>
2023-11-16 13:36:36 +0200

    The parts1-6 -miri Frankel and shira Rosen worked together step by step

--- dotNet5783_6836_4770/DalFacade/DO/Enums.cs
+++ dotNet5783_6836_4770/DalFacade/DO/Enums.cs
@@ -11,8 +11,30 @@ public record Engineer
 (
     int Id,
     int Level,
-    double Cost,
-    string? Name= null,
-    string? Email= null
+    double? Cost = null,
+    string? Name = null,
+    string? Email = null
 );
-
+public record Dependency
+(
+    int Id,
+    int DependenTask,
+    int DependensOnTask
+);
+public record Task
+(
+   int Id ,
+   bool IsMileston,
+   DateTime CreateAtDate,
+   DateTime StartDate,
+   DateTime ScheduledDate,
+   DateTime DeadlineDate,
+   DateTime CompleteDate,
+   TimeSpan RequiredEffortTime,
+   int EngineerId,
+   string? Deliverables=null,
+   string? Remarks=null,
+   int? CopmlexityLevel=null,
+   string? Description=null,
+   string? Alias=null
+);
---
+++ dotNet5783_6836_4770/DalList/DataSource.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Dal;
+
+internal class DataSource
+{
+    internal static List<DO.Engineer> Engineers { get; } = new();
+    internal static List<DO.Dependency> Dependencies { get; } = new();
+    internal static List<DO.Task> Tasks { get; } = new();
+    internal static class Config
+    {
+        //id for dependency entity
+        internal const int startDepenId = 10;
+        private static int nextDepenId = startDepenId;
+        internal static int NextDepenId { get => nextDepenId++; }
+        //id for Task entity
+        internal const int startTaskId = 10;
+        private static int nextTaskId = startTaskId;
+        internal static int NextTaskId { get => nextTaskId++; }
+    }
+
+
+}
