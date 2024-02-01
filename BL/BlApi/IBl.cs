1bca0adaf08c1fd7b0c3d8db61886c52c711ac2c
yafi Rozental <shira.r200@gmail.com>
2024-02-01 23:17:02 +0200

    checking fetch

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -11,12 +11,8 @@ namespace BlApi;
 public interface IBl
 {
     public IEngineer Engineer { get; }  
-    public IEngineerInTask EngineerInTask { get; }  
     public IMileStone MileStone { get; }
-    public IMilestoneInList MilestoneInList { get; }
-    public IMilestoneInTask MilestoneInTask { get; }    
     public  ITask Task { get; }
-    public ITaskInEngineer TaskInEngineer { get; }
-    public ITaskInList TaskInList { get; }
+    
 
 }
