797566133fde2a7da00b5b3928ba314fa35cf402
Miri Frenkel <mifrenke@acad.jct.ac.il>
2024-04-02 22:42:55 +0300

    fixing the clock page

--- PL/Admin.xaml.cs
+++ PL/Admin.xaml.cs
@@ -25,7 +25,7 @@ namespace PL
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-        public static readonly DependencyProperty CurrentTimeProperty = DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow), new PropertyMetadata(default(DateTime)));
+        public static readonly DependencyProperty CurrentTimeProperty = DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(Admin), new PropertyMetadata(default(DateTime)));
         public DateTime CurrentTime
         {
             get { return (DateTime)GetValue(CurrentTimeProperty); }
